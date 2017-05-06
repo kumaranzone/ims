bf11032dae08de5af62d546d84477eacc55bddd4
Kumaran M <mmurugesan@tekion.com>
2017-05-06 14:01:28 +0530

    Replica of au dk info

--- src/client/app/shared/footer/footer.component.ts
+++ src/client/app/shared/footer/footer.component.ts
@@ -6,6 +6,7 @@ import { Component } from '@angular/core';
 @Component({
   moduleId: module.id,
   selector: 'sd-footer',
-  templateUrl: 'footer.component.html'
+  templateUrl: 'footer.component.html',
+  styleUrls: ['footer.component.css'],
 })
 export class FooterComponent { }
